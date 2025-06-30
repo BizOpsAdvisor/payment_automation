92b8cf1fe0bf310bcf4b16c3964e870a0414155b
BizOpsAdvisor <146766321+BizOpsAdvisor@users.noreply.github.com>
2025-06-30 19:05:20 +0500

    Add OAuth proxy endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,9 @@ def create_app():
     app = Flask(__name__)
 
     from .routes.invoice import invoice_bp
+    from .routes.oauth import oauth_bp
+
     app.register_blueprint(invoice_bp, url_prefix="/log-invoice")
+    app.register_blueprint(oauth_bp)
 
     return app
